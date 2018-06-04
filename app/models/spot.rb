13b453788822c0b29ebafdb8e86753b0c52d426a
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2018-06-04 19:47:21 +0200

    validation spots

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,8 @@
 class Spot < ApplicationRecord
+  #validations
+  validates :name, presence: true
+  validates :lat, presence: true
+  validates :long, presence: true
+  validates :adresse, presence: true
+  #associtions
 end
