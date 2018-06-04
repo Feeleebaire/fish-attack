cff5e3217d7f4b4dfdb4267b993343ba5d195772
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2018-06-04 20:38:01 +0200

    geocoder

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,10 @@
 class Spot < ApplicationRecord
+  geocoded_by :addresse
+  after_validation :geocode, if: :will_save_change_to_address?
   #validations
   validates :name, presence: true
-  validates :lat, presence: true
-  validates :long, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
   validates :adresse, presence: true
   #associtions
 end
