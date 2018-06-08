111ba5eaf51c60016b42d3bb708f40ece700d47d
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2018-06-08 16:14:15 +0200

    leurre model

---
+++ app/models/leurre.rb
@@ -0,0 +1,4 @@
+class Leurre < ApplicationRecord
+  belongs_to :leurre_type
+  belongs_to :type_fish
+end
---
+++ test/models/leurre_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LeurreTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
